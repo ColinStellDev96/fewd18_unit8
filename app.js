485f55d15859b66bf1b66690d73d47732de8b234
Colin Stell <colinjstell@gmail.com>
2019-01-10 20:28:38 -0700

    displaying data on html page

--- app.js
+++ app.js
@@ -11,18 +11,23 @@ fetch('https://randomuser.me/api/?results=12&inc=name,location,email,phone,pictu
         console.log(data);
         data.results.forEach(function (employee) {
             console.log(employee);
+            const caps = function (attr) {
+                return attr.charAt(0).toUpperCase() + attr.slice(1);
+            };
+            const firstName = employee.name.first;
+            const lastName = employee.name.last;
+            const city = employee.location.city;
             const html =
-                `
+            `
             <div class="employee-card">
             <img src="${employee.picture.large}" alt="employee_image">
                 <div class="employee-info">
-                    <div class="name">Employee Name</div>
-                    <div class="email">Email</div>
-                    <div class="city">City</div>
+                    <div class="name">${caps(firstName) + ' ' + caps(lastName)}</div>
+                    <div class="email">${employee.email}</div>
+                    <div class="city">${caps(city)}</div>
                 </div>
             </div>
             `;
-            return directory.innerHTML = html;
+            directory.innerHTML += html;
         });
-
     })
