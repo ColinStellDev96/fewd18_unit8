9dc1e500804a9aefa6fded846ebbab7f02349a57
Colin Stell <colinjstell@gmail.com>
2019-01-13 21:54:26 -0700

    pulling date into modal

--- app.js
+++ app.js
@@ -21,14 +21,32 @@ fetch('https://randomuser.me/api/?results=12&inc=name,location,email,phone,pictu
             const lastName = employee.name.last;
             const city = employee.location.city;
             const html = `
-            <div id="employee-card">
-            <img src="${employee.picture.large}" alt="employee_image">
-                <div class="employee-info">
+            <div class="modal">
+            <input id="modal__trigger" type="checkbox">
+            <label for="modal__trigger">
+                <div id="employee-card">
+                    <img src="${employee.picture.large}" alt="employee_image">
+                    <div class="employee-info">
+                        <div class="name">${caps(firstName) + ' ' + caps(lastName)}</div>
+                        <div class="email">${employee.email}</div>
+                        <div class="city">${caps(city)}</div>
+                    </div>
+                </div>
+            </label>
+            <div class="modal__overlay">
+                <div class="modal__wrap">
+                    <label for="modal__trigger">&#10006;</label>
+                    <img src="${employee.picture.large}" alt="employee_image">
                     <div class="name">${caps(firstName) + ' ' + caps(lastName)}</div>
                     <div class="email">${employee.email}</div>
                     <div class="city">${caps(city)}</div>
+                    <hr>
+                    <div class="phone">${employee.phone}</div>
+                    <div class="address">${employee.location.street + ',' + ' ' + employee.location.state + ' ' + employee.location.postcode}</div>
+                    <div class="dob">Birthday: ${employee.dob.date}</div>
                 </div>
             </div>
+        </div>
             `;
             directory.innerHTML += html;
         });
