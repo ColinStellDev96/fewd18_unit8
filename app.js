cf72a4cd6e0db491744fb6dd20f682eb339cb7a7
Colin Stell <colinjstell@gmail.com>
2019-01-15 20:27:42 -0700

    re-worked employee card JS to be more intuative

--- app.js
+++ app.js
@@ -17,26 +17,31 @@ fetch('https://randomuser.me/api/?results=12&inc=name,location,email,phone,pictu
             const caps = function (attr) {
                 return attr.charAt(0).toUpperCase() + attr.slice(1);
             };
+
+            const photo = employee.picture.large;
             const firstName = employee.name.first;
             const lastName = employee.name.last;
             const city = employee.location.city;
-            const html = `
-                <div class="employee-card">
-                    <img src="${employee.picture.large}" alt="employee_image">
-                    <div class="employee-info">
-                        <div class="name">${caps(firstName) + ' ' + caps(lastName)}</div>
-                        <div class="email">${employee.email}</div>
-                        <div class="city">${caps(city)}</div>
-                    </div>
-                </div>
+            const email = employee.email;
+
+            const card = document.createElement('div')
+            card.className = 'employee-card';
+            card.innerHTML = `
+            <img src="${photo}" alt="employee_image">
+            <div class="employee-info">
+                <div class="name">${caps(firstName) + ' ' + caps(lastName)}</div>
+                <div class="email">${email}</div>
+                <div class="city">${caps(city)}</div>
+            </div>
             `;
-            directory.innerHTML += html;
+            directory.append(card);
         });
-    });
+    }); // END FETCH DATA
 
-toggleModal = () => {
-    modal.classList.toggle('show-modal');
-}
+modalUp.addEventListener('click', function () {
+    modal.classList.add('show-modal');
+});
 
-modalUp.addEventListener('click', toggleModal());
-closeModal.addEventListener('click', toggleModal());
+closeModal.addEventListener('click', function () {
+    modal.classList.remove('show-modal');
+});
