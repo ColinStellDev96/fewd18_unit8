b958d54e14024982747d396482e42331af13de8c
Colin Stell <colinjstell@gmail.com>
2019-01-21 18:57:02 -0700

    modul js work, not complete

--- app.js
+++ app.js
@@ -1,67 +1,79 @@
-const directory = document.getElementById('directory');
-const employeeCard = document.getElementsByClassName('employee-card')[0];
-const modal = document.querySelector('.modal');
-const closeModal = document.querySelector('.close-modal');
+const directory = document.getElementById("directory");
+const employeeCard = document.getElementsByClassName("employee-card")[0];
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector(".close-modal");
 
-fetch('https://randomuser.me/api/?results=12&inc=name,location,email,phone,picture,dob')
-    .then(response => {
-        return response.json();
-    })
-    .catch(err => {
-        console.log('Error-No Data For You')
-    })
-    .then(data => {
-        console.log(data);
-        data.results.forEach(function (employee) {
-            console.log(employee);
-            const caps = function (attr) {
-                return attr.charAt(0).toUpperCase() + attr.slice(1);
-            };
+fetch(
+  "https://randomuser.me/api/?results=12&inc=name,location,email,phone,picture,dob"
+)
+  .then(response => {
+    return response.json();
+  })
+  .catch(err => {
+    console.log("Error-No Data For You");
+  })
 
-            const photo = employee.picture.large;
-            const firstName = employee.name.first;
-            const lastName = employee.name.last;
-            const city = employee.location.city;
-            const email = employee.email;
+  .then(data => {
+    console.log(data);
+    data.results.forEach(function(employee, i) {
+      console.log(employee);
 
-            const card = document.createElement('div')
-            card.className = 'employee-card';
-            card.innerHTML = `
+      const caps = function(attr) {
+        return attr.charAt(0).toUpperCase() + attr.slice(1);
+      };
+
+      const photo = employee.picture.large;
+      const firstName = employee.name.first;
+      const lastName = employee.name.last;
+      const name = caps(firstName) + " " + caps(lastName);
+      const city = caps(employee.location.city);
+      const email = employee.email;
+      const phone = employee.phone;
+      const address =
+        caps(employee.location.street) +
+        "," +
+        " " +
+        caps(employee.location.state) +
+        " " +
+        employee.location.postcode;
+      const birthday = employee.dob;
+
+      const card = document.createElement("div");
+      card.className = "employee-card";
+      card.innerHTML = `
             <img src="${photo}" alt="employee_image">
             <div class="employee-info">
-                <div class="name">${caps(firstName) + ' ' + caps(lastName)}</div>
+                <div class="name">${name}</div>
                 <div class="email">${email}</div>
-                <div class="city">${caps(city)}</div>
+                <div class="city">${city}</div>
             </div>
             `;
-            directory.append(card);
-        });
-
-        const modalContent = document.createElement('div');
-        modalContent.className = 'modal-content';
+      directory.append(card);
+    });
+    const modalContent = document.createElement("div");
+        modalContent.className = "modal-content";
+        modalContent.innerHTML = `
+            <div class="modal-content">
+                <span class="close-modal">&#10006;</span>
+                <img src="${photo}" alt="employee_image">
+                <div class="name">${name}</div>
+                <div class="email">${email}</div>
+                <div class="city">${city}</div>
+                <hr>
+                <div class="phone">${phone}</div>
+                <div class="address">${address}</div>
+                <div class="dob">Birthday: ${birthday}</div>
+            </div>
+            `;
         modal.append(modalContent);
+  
+    employeeCard.addEventListener("click", function() {
+        modal.classList.add("show-modal");
+      });
+      
+      closeModal.addEventListener("click", function() {
+        modal.classList.remove("show-modal");
+      });
+  }); // END FETCH DATA
 
 
-        `
-        <div class="modal-content">
-            <span class="close-modal">&#10006;</span>
-            <img src="https://via.placeholder.com/150" alt="employee_image">
-            <div class="name">First Last</div>
-            <div class="email">email</div>
-            <div class="city">city</div>
-            <hr>
-            <div class="phone">phone</div>
-            <div class="address">adress</div>
-            <div class="dob">Birthday: 00/00/0000</div>
-        </div>
-        `
-
-    }); // END FETCH DATA
-
-employeeCard.addEventListener('click', function () {
-    modal.classList.add('show-modal');
-});
-
-closeModal.addEventListener('click', function () {
-    modal.classList.remove('show-modal');
-});
