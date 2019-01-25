7f4f4d32d8490fd10ec42faa469777add0983c76
Colin Stell <colinjstell@gmail.com>
2019-01-24 22:09:13 -0700

    fixed dynamic modal

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const directory = document.getElementById("directory");
-const modal = document.querySelector(".modal");
 let employeeCard;
-let closeModal;
 
-// Create Modal Content Div
-const modalContent = document.createElement("div");
-modalContent.className = "modal-content";
+const caps = function(attr) {
+    return attr.charAt(0).toUpperCase() + attr.slice(1);
+  };
 
 fetch(
   "https://randomuser.me/api/?results=12&inc=name,location,email,phone,picture,dob"
@@ -21,25 +19,21 @@ fetch(
     data.results.forEach(function(employee, i) {
       console.log(employee);
 
-      const caps = function(attr) {
-        return attr.charAt(0).toUpperCase() + attr.slice(1);
-      };
-
-      const photo = employee.picture.large;
-      const firstName = employee.name.first;
-      const lastName = employee.name.last;
-      const name = caps(firstName) + " " + caps(lastName);
-      const city = caps(employee.location.city);
-      const email = employee.email;
-      const phone = employee.phone;
-      const address =
+      let photo = employee.picture.large;
+      let firstName = employee.name.first;
+      let lastName = employee.name.last;
+      let name = caps(firstName) + " " + caps(lastName);
+      let city = caps(employee.location.city);
+      let email = employee.email;
+      let phone = employee.phone;
+      let address =
         caps(employee.location.street) +
         "," +
         " " +
         caps(employee.location.state) +
         " " +
         employee.location.postcode;
-      const birthday = employee.dob.date;
+      let birthday = employee.dob.date;
 
       const card = document.createElement("div");
       card.className = "employee-card";
@@ -52,33 +46,62 @@ fetch(
             </div>
             `;
       directory.append(card);
-
-      modalContent.innerHTML = `
-                <span class="close-modal">&#10006;</span>
-                <img src="${photo}" alt="employee_image">
-                <div class="name">${name}</div>
-                <div class="email">${email}</div>
-                <div class="city">${city}</div>
-                <hr>
-                <div class="phone">${phone}</div>
-                <div class="address">${address}</div>
-                <div class="dob">Birthday: ${birthday}</div>
-            `;
-      modal.append(modalContent);
     });
 
     employeeCard = document.getElementsByClassName("employee-card");
     closeModal = document.querySelector(".close-modal");
     console.log(employeeCard);
 
-    for (var i = 0; i < employeeCard.length; i++) {
+    for (let i = 0; i < employeeCard.length; i++) {
       employeeCard[i].addEventListener("click", function() {
-        console.log("click");
         modal.classList.add("show-modal");
+        modalInfo(data.results, data.results[i], i);
       });
     }
-
-    closeModal.addEventListener("click", function() {
-      modal.classList.remove("show-modal");
-    });
   }); // END FETCH DATA
+
+// Create Modal Content Div
+const modalContent = document.createElement("div");
+modalContent.className = "modal-content";
+
+const modal = document.querySelector(".modal");
+let closeModal;
+
+const modalInfo = (employees, employee, index) => {
+    let photo = employee.picture.large;
+    let firstName = employee.name.first;
+    let lastName = employee.name.last;
+    let name = caps(firstName) + " " + caps(lastName);
+    let city = caps(employee.location.city);
+    let email = employee.email;
+    let phone = employee.phone;
+    let address =
+      caps(employee.location.street) +
+      "," +
+      " " +
+      caps(employee.location.state) +
+      " " +
+      employee.location.postcode;
+    let birthday = employee.dob.date;
+
+  let modalInternal = `
+      <span class="close-modal">&#10006;</span>
+      <img src="${photo}" alt="employee_image">
+      <div class="name">${name}</div>
+      <div class="email">${email}</div>
+      <div class="city">${city}</div>
+      <hr>
+      <div class="phone">${phone}</div>
+      <div class="address">${address}</div>
+      <div class="dob">Birthday: ${birthday}</div>
+      `;
+  modalContent.innerHTML = modalInternal;
+
+  modal.append(modalContent);
+
+  closeModal = document.querySelector('.close-modal');
+  
+  closeModal.addEventListener("click", function() { 
+    modal.classList.remove("show-modal");
+  });
+};
